2e7b587443ea79f27cbd2b279b74d1cef4d99845
isabela-coder <81197670+isabela-coder@users.noreply.github.com>
2021-03-27 00:23:18 -0300

    Aula 22-03 - Pacotes MVC criados e Formulario CADPESSOA

---
+++ src/exercicio13/Exercicio13.java
@@ -0,0 +1,26 @@
+
+package exercicio13;
+
+import java.util.Scanner;
+
+
+public class Exercicio13 {
+
+    
+    public static void main(String[] args) {
+        
+        
+         float Salario;
+        double novosalario = 0;
+        Scanner leia = new Scanner (System.in);
+         System.out.println("Digite o seu salario");
+         Salario =Float.parseFloat(leia.nextLine());
+         if (Salario >1);
+         novosalario = Salario+(Salario+0*25);
+         
+         System.out.println(novosalario);
+    }
+}
+    
+    
+
